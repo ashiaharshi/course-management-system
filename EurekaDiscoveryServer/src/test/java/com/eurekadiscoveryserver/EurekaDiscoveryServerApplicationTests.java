dc905fe6f1a58742136d27c1d3dfb7ecded9bede
Harshitha S <81668171+ashiaharshi@users.noreply.github.com>
2023-06-05 23:19:55 +0530

    Eureka Discovery Server

---
+++ EurekaDiscoveryServer/src/test/java/com/eurekadiscoveryserver/EurekaDiscoveryServerApplicationTests.java
@@ -0,0 +1,14 @@
+//package com.eurekadiscoveryserver;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class EurekaDiscoveryServerApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//}
